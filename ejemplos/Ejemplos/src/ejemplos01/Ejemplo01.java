4902b0189de0643dbefbf69c1b8702fa31f724a7
Cody <codycabreraa@gmail.com>
2024-01-03 16:10:28 -0500

    Cambios

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -15,21 +15,43 @@ public class Ejemplo01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
-        String miCiudad = obtenerCiudadMayuscula("Loja");
-        System.out.printf("%s\n", miCiudad);
+        // si el metodo tiene en su cabecera la palabra void significa que es 
+        // un procedimiento
+        // String miCiudad = obtenerCiudadMayuscula("Loja");
+        obtenerMultiplicacion(2, 10); // asi se invoca la funcion
+        String miMensaje = obtenerMultiplicacionDos(7, 50);
+        // se tiene que declarar el mismo tipo de variable o se lo puede 
+        // convertir Ej: int a = (int)c+b
+        System.out.println(miMensaje);
+        // System.out.printf("%s\n", miCiudad);
+       
+    }
+    public static String obtenerMultiplicacionDos(int tabla, int limite){
+        int valor;
+        String cadena = "";
+        for (int i = 0; i < limite; i++) {
+            valor = tabla * i;
+            cadena = String.format("%s%d * %d = %d\n", cadena, 
+                    tabla, i, valor);
+        }
+        return cadena;
     }
     
-    
-    public String obtenerCiudadMayuscula(String m){
+    public static String obtenerCiudadMayuscula(String m){
         String m2 = m.toUpperCase();
         return m2;
     }
     
     public static void obtenerMultiplicacion(int tabla, int limite){
-        
+        String cadena = "";
+        int valor;
+        for (int i = 0; i < limite; i++) {
+            valor = tabla * i;
+            cadena = String.format("%s%d * %d = %d\n", cadena, 
+                    tabla, i, valor);
+            System.out.println(cadena);
+            
+        }
     }
     
     public static String obtenerNombre(){
